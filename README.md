03319b12405f61a1db6caced253c87aae16444e4
pengzh <pengzh@ie.cuhk.edu.hk>
2020-07-29 14:25:51 +0800

    upload everything

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-test script
+# Knowledge Base of Bolei's Group
+
+:camera_flash: [Generative Modeling](#) | :robot: [Reinforcement Learning](#) | :eye: [Visual Recognition](#)
+
+* 300 Characters per line (including blank)
+
+## Quick Start
+
+Hi there! Welecome to the knowledge base of Bolei's Group. For a contributor who wish to construct this base together, please follow these principles:
+
+:desktop_computer:  Do everything at Github website. You don't have to create a PR, a simple online commit is OK.
+
+:notebook_with_decorative_cover: Please write a `README.md` file as the main page of your materials.
+
+:up: Upload your materials via a self-contained folder, including figs, paper link and so on. Please make sure the figures in your markdown files have correct links.
+
+:pencil: CI system will automatically generate the table of contents in this page as long as you have make a new commit via the website.
+
+
+
+## Table of Contents
+
+<!--DIVIDER_DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE-->
+* [Generative Modeling](#)
+* [Reinforcement Learning](#)
+* [Visual Recognition](#)
--- run.py
+++ run.py
@@ -1,9 +1,94 @@
-import sys
-print(sys.executable)
-
-
 import os
+import sys
+
+print(sys.executable)
 print(os.path.abspath("."))
 
-with open("README.md", "w") as f:
-  f.write("test script")
+INDENT = "    "
+MAX_LINE_WIDTH = 300
+
+
+def listdir(path):
+    ret = dict()
+    for p in os.listdir(path):
+        if p.startswith("."):
+            continue
+        abs_path = os.path.join(path, p)
+        if os.path.isdir(abs_path):
+            print(abs_path)
+            ret[p] = listdir(abs_path)
+        else:
+            continue
+    return ret
+
+
+def _parse_folders(folders):
+    lines = []
+
+    folders = {k: folders[k] for k in sorted(folders.keys())}
+    if not folders:
+        return []
+
+    for k, v in folders.items():
+        l = "* [{}](#)".format(k)
+
+        sub_folder_lines = _parse_folders(v)
+        sub_folder_lines = [INDENT + l for l in sub_folder_lines]
+
+        lines.append(l)
+        lines.extend(sub_folder_lines)
+
+    return lines
+
+
+def parse_folders(folders):
+    new_lines = _parse_folders(folders)
+    new_lines = [l[:MAX_LINE_WIDTH] + "\n" for l in new_lines]
+    return new_lines
+
+
+def parse_existing(file_name):
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+    existing_lines = []
+    detected = False
+    for line in lines:
+        existing_lines.append(line)
+        if "DIVIDER_DO_NOT_EDIT_ANYTHING_BELOW_THIS_LINE" in line:
+            detected = True
+            break
+    assert detected, "The divider line is deleted!"
+    return existing_lines
+
+
+def main():
+    # Scan all subfolders
+    folders = listdir(".")
+
+    # Collect all informations
+    new_lines = parse_folders(folders)
+
+    # Read the headers of the file
+    file_name = "README.md"
+    existing_lines = parse_existing(file_name)
+
+    # Write lines to file
+    write_lines = existing_lines + new_lines
+    with open(file_name, "w") as f:
+        f.writelines(write_lines)
+
+    print("Finished writing files! Previous number of lines {}, now {}.".format(
+        len(lines), len(write_lines)
+    ))
+
+
+if __name__ == '__main__':
+    # main()
+    print("===")
+    ret = listdir(".")
+    print("===")
+    lines = parse_folders(ret)
+    for l in lines:
+        print(l)
+    print("===")
+    main()
